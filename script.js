9e57bbb59d6134c1153ad69376bac25595458591
n00202499 <98826054+n00202499@users.noreply.github.com>
2022-02-01 15:26:34 +0000

    week1day1

---
+++ script.js
@@ -0,0 +1,24 @@
+let distance=4
+//declaring a variable//
+//= assaignment operator assigns the variable//
+
+String "john" //letters//
+Float 1.32 //numbers//
+integer 3 //solid number//
+boolean true //true or false 1 or 0//
+array [] //list//
+
+names = ["david", "mary", "peter"]
+numbers = [123,321,123]
+ages = [21,23,42]
+Object{}
+
+someFuntion() //calling a function//
+
+function clap(){ //declaring a function//
+    make a clap noise
+}
+
+rect (12,12,12,12) //perameters//
+
+
---
+++ sketch.js
@@ -0,0 +1,10 @@
+
+//function with a perameter//
+clap(3,8);
+
+function clap(startNum, endNumb){
+    let loopCount = endNum-startNum
+    for(let i=0;i<=loopount;i++){
+    console.log("Hi" + (startNum+i))
+    }
+}
